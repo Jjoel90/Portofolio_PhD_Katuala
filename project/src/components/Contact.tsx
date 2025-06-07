5c7d6c1fe63b61f4ebaab85110a05a664b7a440e
Jjoel90 <joelilunga090@gmail.com>
2025-06-07 11:38:41 +0200

    retour vers publications

--- project/src/components/Contact.tsx
+++ project/src/components/Contact.tsx
@@ -179,7 +179,7 @@ export const Contact = () => {
                   CV - KATUALA GIZE Regis
                 </a>
                 <a 
-                  href="#" 
+                  href="#publications" 
                   className="flex items-center text-amber-700 hover:text-amber-800"
                 >
                   <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
