d29b958eef30e1f57a9fbaef3aba811d8721a714
Jjoel90 <joelilunga090@gmail.com>
2025-06-08 18:03:31 +0200

    suppressions voir plus des publications rubrique publications

--- project/src/components/Publications.tsx
+++ project/src/components/Publications.tsx
@@ -186,17 +186,6 @@ export const Publications = () => {
           ))}
         </div>
         
-        <div className="text-center mt-10">
-          <a 
-            href="#publications" 
-            className="inline-flex items-center text-amber-700 hover:text-amber-800 font-medium"
-          >
-            Voir toutes les publications
-            <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-            </svg>
-          </a>
-        </div>
       </div>
     </section>
   );
